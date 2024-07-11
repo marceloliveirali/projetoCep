ecec755848af49dbd0aeafbfd700de10417a6b78
Marcelo de Oliveira Lima <marceloliveirali@gmail.com>
2024-07-11 18:10:25 -0300

    projeto cep

---
+++ main.js
@@ -0,0 +1,34 @@
+/*window.document.addEventListener('DOMContentLoaded', function() {
+    window.document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+        const xhttp = new XMLHttpRequest();
+        const cep = window.document.getElementById('cep').value;
+        const endPoint = `http://viacep.com.br/ws/${cep}/json`;
+        xhttp.open('GET', endPoint);
+        xhttp.send();
+    });
+ });*/
+
+ $(Window.document).ready(function() {
+    $('#cep').mask('00000-000');
+    $('#btn-buscar-cep').click(function() {
+        const cep = $('#cep').val();
+        const endPoint =`http://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
+
+        $.ajax(endPoint).done(function(resposta) {
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 1000);
+        });
+    })
+ });
